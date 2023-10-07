6e4592c52c9e40ec30e47a5cca6ba8ccec656bfe
a3ylf <alexandre3ylf@gmail.com>
2023-10-07 01:07:57 -0300

    Lp2

---
+++ src/Client.java
@@ -0,0 +1,22 @@
+
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+import java.util.concurrent.*;
+
+public class Client {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        Socket s = new Socket("localhost", 6969);
+        DataInputStream in = new DataInputStream(s.getInputStream());
+        DataOutputStream out = new DataOutputStream(s.getOutputStream());
+        System.out.println(in.readUTF());
+        while(true){
+            String fromuser = sc.nextLine();
+            out.writeUTF(fromuser);
+            String fromserver = in.readUTF();
+            if (fromserver.equals("break")) break;
+            else System.out.println(fromserver);
+    }
+    }
+}
