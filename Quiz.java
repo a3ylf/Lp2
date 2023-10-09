c4452fb8e09193a10eb587a33ba29681dac86f6c
pedro.lima <pedro.henrique.araujo.lima@academico.ufpb.br>
2023-10-09 20:46:09 -0300

    Update Quiz.java

--- Quiz.java
+++ Quiz.java
@@ -3,6 +3,7 @@ import java.io.*;
 
 public class Quiz {
 
+
     public static void quiz(DataInputStream in, DataOutputStream out) throws IOException {
 
         Questions[] questionArray = new Questions[50];
@@ -18,29 +19,29 @@ public class Quiz {
         String userInput;
         out.writeUTF("""
                 Porfavor escolha a dificuldade:
-                1-facil (6 perguntas fáceis, 3 perguntas médias e 1 dificil)
-                2-média (3 perguntas fáceis, 5 perguntas médias e 2 dificeis)
-                3-dificil (2 perguntas fáceis, 4 perguntas médias e 4 dificeis)
-                4-muito dificil (1 pergunta fácil, 3 perguntas médias e 6 dificil)
+                1-facil (6 perguntas fáceis, 3 perguntas médias e 1 difícil)
+                2-média (3 perguntas fáceis, 5 perguntas médias e 2 difíceis)
+                3-dificil (2 perguntas fáceis, 4 perguntas médias e 4 difíceis)
+                4-muito dificil (1 pergunta fácil, 3 perguntas médias e 6 difíceis)
                 """);
 
          do {
             userInput = in.readUTF();
             switch (userInput) {
                 case "1" -> {
-                    easyquiz();
+                    easyquiz(in,out);
                 }
 
                 case "2" -> {
-                    mediumquiz();
+                    mediumquiz(in,out);
                 }
 
                 case "3" -> {
-                    hardquiz();
+                    hardquiz(in,out);
                 }
 
                 case "4" -> {
-                    veryhardquiz();
+                    veryhardquiz(in,out);
                 }
 
 
@@ -52,10 +53,137 @@ public class Quiz {
 
 
 
-    private static void easyquiz() {}
-    private static void mediumquiz() {}
-    private static void hardquiz() {}
-    private static void veryhardquiz() {}
+    private static int easyquiz(DataInputStream in, DataOutputStream out) throws IOException {
+        int pontuacao = 0;
+        for (int i = 0; i < 6; i++) {
+            out.writeUTF(Questions.easyQuestions[i].getQuestion());
+          String userAnswer = in.readUTF();
+            if ((Questions.easyQuestions[i].getAnswer()).equals(userAnswer)) {
+                pontuacao++;
+                out.writeUTF("Resposta correta");
+            } else {
+                out.writeUTF("Resposta errada");
+            }
+        }
+        for(int i=0;i<3;i++){
+            out.writeUTF(Questions.mediumQuestions[i].getQuestion());
+           String userAnswer = in.readUTF();
+            if ((Questions.mediumQuestions[i].getAnswer()).equals(userAnswer)) {
+                pontuacao++;
+                out.writeUTF("Resposta correta");
+            } else {
+                out.writeUTF("Resposta errada");
+            }
+        }
+        out.writeUTF(Questions.hardQuestions[0].getQuestion());
+        String userAnswer = in.readUTF();
+        if (Questions.hardQuestions[0].getAnswer().equals(userAnswer)) {
+            pontuacao++;
+            out.writeUTF("Resposta correta");
+        } else {
+            out.writeUTF("Resposta errada");
+        }
+        return pontuacao;
+
+    }
+    private static int mediumquiz(DataInputStream in, DataOutputStream out) throws IOException {
+        int pontuacao = 0;
+        for (int i = 0; i < 3; i++) {
+            out.writeUTF(Questions.easyQuestions[i].getQuestion());
+            String userAnswer = in.readUTF();
+            if (Questions.easyQuestions[i].getAnswer().equals(userAnswer)) {
+                pontuacao++;
+                out.writeUTF("Resposta correta");
+            } else {
+                out.writeUTF("Resposta errada");
+            }
+        }
+        for(int i=0;i<5;i++){
+            out.writeUTF(Questions.mediumQuestions[i].getQuestion());
+            String userAnswer = in.readUTF();
+            if (Questions.mediumQuestions[i].getAnswer().equals(userAnswer)) {
+                pontuacao++;
+                out.writeUTF("Resposta correta");
+            } else {
+                out.writeUTF("Resposta errada");
+            }
+        }
+        for(int i=0;i<2;i++){
+        out.writeUTF(Questions.hardQuestions[i].getQuestion());
+        String userAnswer = in.readUTF();
+        if (Questions.hardQuestions[i].getAnswer().equals(userAnswer)) {
+            pontuacao++;
+            out.writeUTF("Resposta correta");
+        } else {
+            out.writeUTF("Resposta errada");
+        }}
+        return pontuacao;}
+    private static int hardquiz(DataInputStream in, DataOutputStream out) throws IOException {
+        int pontuacao = 0;
+
+        for (int i = 0; i < 2; i++) {
+            out.writeUTF(Questions.easyQuestions[i].getQuestion());
+            String userAnswer = in.readUTF();
+            if (Questions.easyQuestions[i].getAnswer().equals(userAnswer)) {
+                pontuacao++;
+                out.writeUTF("Resposta correta");
+            } else {
+                out.writeUTF("Resposta errada");
+            }
+        }
+        for(int i=0;i<4;i++){
+            out.writeUTF(Questions.mediumQuestions[i].getQuestion());
+            String userAnswer = in.readUTF();
+            if (Questions.mediumQuestions[i].getAnswer().equals(userAnswer)) {
+                pontuacao++;
+                out.writeUTF("Resposta correta");
+            } else {
+                out.writeUTF("Resposta errada");
+            }
+        }
+        for(int i=0;i<4;i++){
+        out.writeUTF(Questions.hardQuestions[i].getQuestion());
+        String userAnswer = in.readUTF();
+        if (Questions.hardQuestions[i].getAnswer().equals(userAnswer)) {
+            pontuacao++;
+            out.writeUTF("Resposta correta");
+        } else {
+            out.writeUTF("Resposta errada");
+        }}
+        return pontuacao;
+    }
+    private static int veryhardquiz(DataInputStream in, DataOutputStream out) throws IOException {
+        int pontuacao = 0;
+        
+        for(int i=0;i<4;i++){
+            out.writeUTF(Questions.mediumQuestions[i].getQuestion());
+            String userAnswer = in.readUTF();
+            if (Questions.mediumQuestions[i].getAnswer().equals(userAnswer)) {
+                pontuacao++;
+                out.writeUTF("Resposta correta");
+            } else {
+                out.writeUTF("Resposta errada");
+            }
+        }
+        for(int i=0;i<4;i++){
+        out.writeUTF(Questions.hardQuestions[i].getQuestion());
+        String userAnswer = in.readUTF();
+        if (Questions.hardQuestions[i].getAnswer().equals(userAnswer)) {
+            pontuacao++;
+            out.writeUTF("Resposta correta");
+        } else {
+            out.writeUTF("Resposta errada");
+        }}
+        
+    
+        out.writeUTF(Questions.easyQuestions[0].getQuestion());
+        String userAnswer = in.readUTF();
+        if (Questions.easyQuestions[0].getAnswer().equals(userAnswer)) {
+            pontuacao++;
+            out.writeUTF("Resposta correta");
+        } else {
+            out.writeUTF("Resposta errada");
+        }return pontuacao;}
 
 
 
