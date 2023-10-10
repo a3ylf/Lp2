464cb9e8b56254b5456c495f72424a9bd2a7afe0
pedro.lima <pedro.henrique.araujo.lima@academico.ufpb.br>
2023-10-10 09:59:17 -0300

    Update Questions.java

--- Questions.java
+++ Questions.java
@@ -3,9 +3,9 @@ public class Questions {
     private final String difficulty;
     private final String answer;
     private final String question;
-    public static Questions easyQuestions[] = new Questions[6];
-    public static Questions hardQuestions[] = new Questions[6];
-    public static Questions mediumQuestions[] = new Questions[4];
+    public static Questions easyQuestions[] = new Questions[10];
+    public static Questions hardQuestions[] = new Questions[10];
+    public static Questions mediumQuestions[] = new Questions[10];
 
     public Questions(String difficulty, String answer, String question) {
         this.difficulty = difficulty;
@@ -14,11 +14,11 @@ public class Questions {
     }
 
     public String getDifficulty() {
-        return difficulty;
+        return this.difficulty;
     }
 
     public String getAnswer() {
-        return answer;
+        return this.answer;
     }
 
     static {
@@ -28,20 +28,34 @@ public class Questions {
             easyQuestions[3] = new Questions("easy", "D", "Qual a quarta letra do alfabeto?");
             easyQuestions[4] = new Questions("easy", "2", "Quanto é 5-3?");
             easyQuestions[5] = new Questions("easy", "F", "Qual a sexta letra do alfabeto?");
-            mediumQuestions[0] = new Questions("medium", "G", "Qual a sétima letra do alfabeto?");
-            mediumQuestions[1] = new Questions("medium", "H", "Qual a oitava letra do alfabeto?");
-            mediumQuestions[2] = new Questions("medium", "I", "Qual a nona letra do alfabeto?");
-            mediumQuestions[3] = new Questions("medium", "J", "Qual a décima letra do alfabeto?");
-            hardQuestions[0] = new Questions("hard", "L", "Qual a décima segunda letra do alfabeto?");
-            hardQuestions[1] = new Questions("hard", "M", "Qual a décima terceira letra do alfabeto?");
-            hardQuestions[2] = new Questions("hard", "N", "Qual a décima quarta letra do alfabeto?");
-            hardQuestions[3] = new Questions("hard", "O", "Qual a décima quinta letra do alfabeto?");
-            hardQuestions[4] = new Questions("hard", "P", "Qual a décima sexta letra do alfabeto?");
-            hardQuestions[5] = new Questions("hard", "Q", "Qual a décima sétima letra do alfabeto?");
+            easyQuestions[6] = new Questions("easy", "G", "Qual a sétima letra do alfabeto?");
+            easyQuestions[7] = new Questions("easy", "H", "Qual a oitava letra do alfabeto?");
+            easyQuestions[8] = new Questions("easy", "I", "Qual a nona letra do alfabeto?");
+            easyQuestions[9] = new Questions("easy", "J", "Qual a décima letra do alfabeto?");
+            mediumQuestions[0] = new Questions("medium", "K", "Qual a décima primeira letra do alfabeto?");
+            mediumQuestions[1] = new Questions("medium", "L", "Qual a décima segunda letra do alfabeto?");
+            mediumQuestions[2] = new Questions("medium", "M", "Qual a décima terceira letra do alfabeto?");
+            mediumQuestions[3] = new Questions("medium", "N", "Qual a décima quarta letra do alfabeto?");
+            mediumQuestions[4] = new Questions("medium", "O", "Qual a décima quinta letra do alfabeto?");
+            mediumQuestions[5] = new Questions("medium", "P", "Qual a décima sexta letra do alfabeto?");
+            mediumQuestions[6] = new Questions("medium", "Q", "Qual a décima sétima letra do alfabeto?");
+            mediumQuestions[7] = new Questions("medium", "R", "Qual a décima oitava letra do alfabeto?");
+            mediumQuestions[8] = new Questions("medium", "S", "Qual a décima nona letra do alfabeto?");
+            mediumQuestions[9] = new Questions("medium", "T", "Qual a vigésima letra do alfabeto?");
+            hardQuestions[0] = new Questions("hard", "U", "Qual a vigésima primeira letra do alfabeto?");
+            hardQuestions[1] = new Questions("hard", "V", "Qual a vigésima segunda letra do alfabeto?");
+            hardQuestions[2] = new Questions("hard", "W", "Qual a vigésima terceira letra do alfabeto?");
+            hardQuestions[3] = new Questions("hard", "X", "Qual a vigésima quarta letra do alfabeto?");
+            hardQuestions[4] = new Questions("hard", "Y", "Qual a vigésima quinta letra do alfabeto?");
+            hardQuestions[5] = new Questions("hard", "Z", "Qual a vigésima sexta letra do alfabeto?");
+            hardQuestions[6] = new Questions("hard", "A", "Qual a primeira letra do alfabeto?");
+            hardQuestions[7] = new Questions("hard", "B", "Qual a segunda letra do alfabeto?");
+            hardQuestions[8] = new Questions("hard", "C", "Qual a terceira letra do alfabeto?");
+            hardQuestions[9] = new Questions("hard", "D", "Qual a quarta letra do alfabeto?");
     }
 
     public String getQuestion() {
-        return question;
+        return this.question;
     }}
     
 
