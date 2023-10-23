3b92307857ab466a4d6b2740b7d2bfe03437eff2
a3ylf <120695058+a3ylf@users.noreply.github.com>
2023-10-23 15:21:41 -0300

    Update ServerQuiz.java

--- ServerQuiz.java
+++ ServerQuiz.java
@@ -1,19 +1,20 @@
 import java.io.*;
 import java.net.*;
-import java.util.concurrent.*;
 import java.util.*;
+import java.util.concurrent.*;
 
 public class ServerQuiz {
     public static void main(String[] args) throws IOException {
         Executor exec = Executors.newCachedThreadPool();
-        try (ServerSocket s = new ServerSocket(6969)) {
-            while (true) {
-                Socket ns = s.accept();
-                exec.execute(new protocolServer(ns));
-            }
+        List<multiplayerRoom> salas = new ArrayList<>(5);
+        ServerSocket s = new ServerSocket(6969);
+        Semaphore roomadd = new Semaphore(1);
+        while (true) {
+            Socket ns = s.accept();
+            exec.execute(new protocolServer(ns,roomadd,salas));
         }
-    
     }
+
 }
 
 class protocolServer implements Runnable {
@@ -21,58 +22,53 @@ class protocolServer implements Runnable {
     DataInputStream in;
     DataOutputStream out;
 
-    public protocolServer(Socket ns) throws IOException {
+    private Semaphore roomadd;
+    private List<multiplayerRoom> salas;
+
+    public protocolServer(Socket ns, Semaphore roomadd, List<multiplayerRoom> salas) throws IOException {
         s = ns;
         in = new DataInputStream(s.getInputStream());
         out = new DataOutputStream(s.getOutputStream());
+        this.roomadd = roomadd;
+        this.salas = salas;
     }
 
     public void run() {
         try {
-            menu(s,in,out);
-        } catch (IOException e) {
+            mode.read(out);
+            out.writeUTF("""
+                            Conexão feita! Bem-vindo ao quiz lp2!
+                    """);
+            menu(in, out, roomadd, salas);
+        } catch (IOException | InterruptedException e) {
             throw new RuntimeException(e);
         }
-    }
-    public static void menu(Socket s, DataInputStream in, DataOutputStream out) throws IOException {
-        out.writeUTF("\nConexão feita! Bem-vindo ao quiz lp2!\n\n" +
-                "Escolha o que você deseja fazer:\n" +
-                "1 - Start\n" +
-                "2 - Ranking\n" +
-                "3 - Leave\n");
 
+    }
+
+    public static void menu(DataInputStream in, DataOutputStream out,Semaphore roomadd, List<multiplayerRoom> salas) throws IOException, InterruptedException {
+
+        mode.write(out);
+        out.writeUTF("""
+                Escolha o que você deseja fazer:
+                1 - Start
+                2 - Ranking
+                3 - Leave
+                """);
         String userInput;
         do {
-            String arq="ranking.txt";
             userInput = in.readUTF();
             switch (userInput) {
-                case "1" -> {
-                     Quiz.quiz(in,out);
-                }
+                case "1" -> Quiz.quiz(in,out,roomadd,salas);
 
-                case "2" -> {
-                    try{
-                        List<String> ranking= new ArrayList<>();
-                        FileReader fileReader= new FileReader(arq);
-                        BufferedReader bufferedReader= new BufferedReader(fileReader);
-                        String line;
-                        while((line= bufferedReader.readLine())!= null){
-                            ranking.add(line);
-                        }
-                        bufferedReader.close();
-                        Collections.sort(ranking);
-                        out.writeUTF("Ranking:\n");
-                        for(int i =0; i<ranking.size(); i++){
-                            out.writeUTF((i+1)+" - "+ranking.get(i));
-                        }
-                    }catch(IOException e){
-                        e.printStackTrace();
-                    }
-                }
-                        case "3" ->
-                        out.writeUTF("break");
+                case "2" -> out.writeUTF("You selected Option 2");
 
-                default -> out.writeUTF("escolha inválida");
+                case "3" -> mode.stop(out);
+
+                default -> {
+                    mode.write(out);
+                    out.writeUTF("escolha inválida");
+                }
             }
         } while (!userInput.equals("1") && !userInput.equals("2") && !userInput.equals("3"));
     }
