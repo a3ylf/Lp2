3c5895bd8b233729d4ce969cbafacfcc90822670
Alexandre Rodrigues <120695058+a3ylf@users.noreply.github.com>
2023-11-06 05:54:00 -0300

    Update Client.java
    colocar para o 2 e o 3

--- Client.java
+++ Client.java
@@ -5,32 +5,44 @@ import java.util.Scanner;
 
 
 public class Client{
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        Socket s = new Socket("localhost", 6969);
-        DataInputStream in = new DataInputStream(s.getInputStream());
-        DataOutputStream out = new DataOutputStream(s.getOutputStream());
-        String fromserver;
-        String mode;
-        String fromuser;
+        try {
+            Socket s = new Socket("localhost", 6969);
+            DataInputStream in = new DataInputStream(s.getInputStream());
+            DataOutputStream out = new DataOutputStream(s.getOutputStream());
+            String fromserver;
+            String mode;
+            String fromuser;
 
-        while(true){
-            mode = in.readUTF();
-            if(mode.equals("break")) break;
+            while (true) {
+                mode = in.readUTF();
+                if (mode.equals("break")) break;
 
-            fromserver = in.readUTF();
-            if(!fromserver.isEmpty())
-                System.out.println(fromserver);
+                fromserver = in.readUTF();
+                if (!fromserver.isEmpty())
+                    System.out.println(fromserver);
 
-            if (mode.equals("write")) {
-                fromuser = sc.nextLine();
-                out.writeUTF(fromuser);
-            }
+                if (mode.equals("write")) {
+                    fromuser = sc.nextLine();
+                    out.writeUTF(fromuser);
+                }
 
-            if (mode.equals("read")){}
+                if (mode.equals("read")) {
+                }
 
+
+            sc.close();
+            s.close();
         }
-        sc.close();
-        s.close();
+        }catch (ConnectException connectException){
+            System.out.println("Erro ao conectar ao servidor");
+        }catch (SocketException socketException){
+            System.out.println("Ouve um erro no servidor, o servidor provavelmente caiu");
+
+        }catch (Exception e){
+            System.out.println("Ouve um erro desconhecido");
+        }
+
     }
 }
